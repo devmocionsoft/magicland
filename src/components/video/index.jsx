5aecd9f0899c9d8cffce9c8902205ed6ec2680c3
devmocionsoft <38898576+devmocionsoft@users.noreply.github.com>
2020-11-17 17:40:12 -0500

    corrigiendo el link por el player de vimeo

--- src/components/video/index.jsx
+++ src/components/video/index.jsx
@@ -7,12 +7,17 @@ function Video() {
 		<>
 			<WrapperLayout>
 				<iframe
-					src="https://vimeo.com/480506602"
+					src="https://player.vimeo.com/video/480450196"
 					title="magicLand"
 					frameborder="0"
 					allow="autoplay; fullscreen"
 					allowfullscreen
 				>
+<iframe src="https://player.vimeo.com/video/480450196?title=0&byline=0&portrait=0" width="640" height="291" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+<p><a href="https://vimeo.com/480450196">Magic Land Trailer</a> from <a href="https://vimeo.com/mocion">Moderador</a> on <a href="https://vimeo.com">Vimeo</a>.</p>					
+					
+					
+					
 				</iframe>
 			</WrapperLayout>
 			<ImageStripe src={stripe} />
