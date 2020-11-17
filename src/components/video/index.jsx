ef2e7e73150999332174c61726e906890fd66c9f
devmocionsoft <38898576+devmocionsoft@users.noreply.github.com>
2020-11-17 17:16:17 -0500

    Update index.jsx
    volviendo a cambiar la url del trailer

--- src/components/video/index.jsx
+++ src/components/video/index.jsx
@@ -7,7 +7,7 @@ function Video() {
 		<>
 			<WrapperLayout>
 				<iframe
-					src="https://vimeo.com/480450196/536a2075c5"
+					src="https://vimeo.com/480450196"
 					title="magicLand"
 					frameborder="0"
 					allow="autoplay; fullscreen"
