5f234eb3bd2068b6559f7a117d0b560d62eb8ed5
devmocionsoft <38898576+devmocionsoft@users.noreply.github.com>
2020-11-19 17:25:30 -0500

    Update index.jsx
    ajuste

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -7,7 +7,7 @@ import slogan from '../../assets/images/SLOGAN.png';
 import divisor from '../../assets/images/DIVISOR.png';
 
 function Header() {
-  const url = 'https://evius.co/landing/5f99a20378f48e50a571e3b6';
+  const url = 'https://themagic.land/';
   return (
     <Container>
       <Section>
@@ -20,7 +20,7 @@ function Header() {
         <ImageSlogan src={slogan} />
         <Separator src={divisor} />
         <Title>ÚNETE Y SÉ PARTE DE ESTE MUNDO MÁGICO</Title>
-        <Pressable href={url} target="_blank"><p>REGISTRATE GRATIS  Y RECIBE MÁS INFORMACIÓN PARA SER PARTE DE ESTE MUNDO MÁGICO</p></Pressable>
+        <Pressable href={url} target="_blank"><p>Espera muy pronto el primer capitulo</p></Pressable>
         <SectionText>
           <SubTitle>La producción más grande de youtubers en habla hispana.</SubTitle>
           <Text>En <b>MAGICLAND</b> Yolo, Nando, Mariana y Panda son los protagonistas de esta serie cinematográfica para todas las edades, la serie con 4 episodios exclusivos, cargados de efectos especiales, magia, música, personajes fantásticos y un gran mensaje dirigido hacia la importancia del cuidado del medio ambiente y el futuro de la humanidad. Además de juegos, fiestas virtuales, premios y momentos de interacción con los <b>YOLO AVENTURAS</b>, únete y se parte de esta nueva comunidad en la tierra de la magia. No te quedes sin vivir esta increíble experiencia.</Text>
