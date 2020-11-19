a8db0047b27f334944301740d4242e924061cab2
devmocionsoft <38898576+devmocionsoft@users.noreply.github.com>
2020-11-19 18:02:16 -0500

    Update index.jsx
    colocando el video

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -20,7 +20,7 @@ function Header() {
         <ImageSlogan src={slogan} />
         <Separator src={divisor} />
         <Title>ÚNETE Y SÉ PARTE DE ESTE MUNDO MÁGICO</Title>
-        <Pressable href={url} target="_blank"><p>Ver capitulo</p></Pressable>
+        <iframe src="https://player.vimeo.com/video/481294238" width="640" height="290" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
         <SectionText>
           <SubTitle>La producción más grande de youtubers en habla hispana.</SubTitle>
           <Text>En <b>MAGICLAND</b> Yolo, Nando, Mariana y Panda son los protagonistas de esta serie cinematográfica para todas las edades, la serie con 4 episodios exclusivos, cargados de efectos especiales, magia, música, personajes fantásticos y un gran mensaje dirigido hacia la importancia del cuidado del medio ambiente y el futuro de la humanidad. Además de juegos, fiestas virtuales, premios y momentos de interacción con los <b>YOLO AVENTURAS</b>, únete y se parte de esta nueva comunidad en la tierra de la magia. No te quedes sin vivir esta increíble experiencia.</Text>
