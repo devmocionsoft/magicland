a6a1da207a7eb88b07476f644c111f3995734045
Juliana Geraldine <geraldine.garcia@mocionsoft.com>
2021-02-01 20:09:04 -0500

    Evento ter,inado

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -14,8 +14,9 @@ function Header() {
       <Section>
         <ImageStripe src={stripe} />
         {/* <Title>REGISTRATE GRATIS PARA VER EL PRIMER EPISODIO</Title> */}
-        <Pressable href={url} target="_blank"><p>DISFRUTA DEL EPISODIO FINAL UTILIZANDO TU CÓDIGO PAGO.</p></Pressable>
-        <Wrapper>
+        {/* <Pressable href={url} target="_blank"><p>DISFRUTA DEL EPISODIO FINAL UTILIZANDO TU CÓDIGO PAGO.</p></Pressable> */}
+        <Pressable target="_blank"><p>TERMINADO.</p></Pressable>
+        {/* <Wrapper>
           <ImageAval src={aval} />
           <ImageLogo src={logo} />
         </Wrapper>
@@ -24,7 +25,7 @@ function Header() {
         {/* <Title>Registrate gratis para ver el primer episodio</Title> */}
         {/* <Pressable href={url} target="_blank"><p>VER EPISODIOS</p></Pressable> */}
         {/* <Pressable href={url} target="_blank"><p>REGISTRATE GRATIS PARA VER EL PRIMER EPISODIO</p></Pressable> */}
-        <SectionText>
+        {/* <SectionText>
           <SubTitle>La producción más grande de youtubers en habla hispana.</SubTitle>
           <Text>En <b>MAGICLAND</b> Yolo, Nando, Mariana y Panda son los protagonistas de esta serie cinematográfica para todas las edades, la serie con 4 episodios exclusivos, cargados de efectos especiales, magia, música, personajes fantásticos y un gran mensaje dirigido hacia la importancia del cuidado del medio ambiente y el futuro de la humanidad. Además de juegos, fiestas virtuales, premios y momentos de interacción con los <b>YOLO AVENTURAS</b>, únete y se parte de esta nueva comunidad en la tierra de la magia. No te quedes sin vivir esta increíble experiencia.</Text>
            <iframe id="inlineFrameExample"
@@ -33,7 +34,7 @@ function Header() {
               src="https://juegomagicland.netlify.app/" scrolling="no" >
           </iframe> 
         </SectionText>
-        <ImageStripe src={stripe} />
+        <ImageStripe src={stripe} />  */}
       </Section>
     </Container>
   );
